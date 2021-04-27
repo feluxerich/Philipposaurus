26c3e7983f6a263e43871389d34ae5c7a29c6222
Feluxerich <felix.waidler@web.de>
2021-04-27 18:30:10 +0200

    improved help command

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def status_task():
 
 
 @client.command(name='help', aliases=['h', 'commands'])
-async def command_list(ctx):
+async def command_list(ctx, *, cog_or_command=None):
     cogs = [f'{cog}' for cog in client.cogs.keys()]
     just_events = ['Reaction', 'Events']
     for removable in just_events:
@@ -42,16 +42,29 @@ async def command_list(ctx):
         title='Help',
         color=0x00ffff
     )
-    for cog in cogs:
-        commands = ""
-        for command in client.get_cog(cog).get_commands():
-            commands += f'{command.name} - {command.description}\n'
-        # this version does not display the subcommands
-        # if you want to display them, do this:
-        # for command in client.get_cog(cog).walk_commands():
-        #   ...
-        help_embed.add_field(name=cog, value=str(commands), inline=False)
-    help_embed.add_field(name='No Category', value='help - Shows this help\nreload - Reloads the bot')
+    if cog_or_command is None:
+        for cog in cogs:
+            commands = ""
+            for command in client.get_cog(cog).get_commands():
+                commands += f'{command.name} - {command.description}\n'[:47] + '...'
+            # this version does not display the subcommands
+            # if you want to display them, do this:
+            # for command in client.get_cog(cog).walk_commands():
+            #   ...
+            help_embed.add_field(name=cog, value=str(commands), inline=False)
+        help_embed.add_field(name='No Category', value='help - Shows this help\nreload - Reloads the bot\n\n'
+                                                       'Type `help <cog | command>` for specificated help')
+    else:
+        if cog_or_command in client.cogs:
+            commands = ''
+            for command in client.get_cog(cog_or_command).get_commands():
+                commands += f'{command.name} - {command.description}\n'[:47] + '...'
+            help_embed.add_field(name=cog_or_command, value=commands)
+        elif command := client.get_command(cog_or_command):
+            command = f'{command.name} - {command.description}'
+            help_embed.add_field(name=cog_or_command.capitalize(), value=command)
+        else:
+            help_embed.add_field(name='Error', value='This is not a category or a command')
     help_embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url)
     await ctx.send(embed=help_embed)
 
