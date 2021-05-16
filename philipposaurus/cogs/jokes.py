8b85715c74252143cba026efa1a84a2183cd5498
Feluxerich <felix.waidler@web.de>
2021-05-16 21:54:13 +0200

    made a nick command just for fun

--- philipposaurus/cogs/jokes.py
+++ philipposaurus/cogs/jokes.py
@@ -1,5 +1,5 @@
 from discord.ext.commands import Cog, command
-from discord import Embed
+from discord import Embed, Member
 from requests import get
 from asyncio import sleep
 from utils import colour
@@ -21,6 +21,24 @@ class Joke(Cog):
         await sleep(10)
         joke_embed.description = joke['punchline']
         await sent_message.edit(embed=joke_embed, delete_after=10)
+
+    @command(aliases=['rename', 'nickname'], description='Change the nickname of a user to a very funny nickname')
+    async def nick(self, ctx, member: Member, *, nickname):
+        if len(nickname) <= 32:
+            await member.edit(nick=nickname)
+            changed_embed = Embed(
+                title='Nickname',
+                description=f'The nickname was changed successfully to {nickname}',
+                colour=colour()
+            )
+            await ctx.send(embed=changed_embed)
+        else:
+            too_long_embed = Embed(
+                title='Nickname',
+                description='The length of the nickname must be fewer than 32',
+                colour=colour()
+            )
+            await ctx.send(embed=too_long_embed)
 
 
 def setup(client):
